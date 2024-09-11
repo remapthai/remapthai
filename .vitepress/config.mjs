e7304136895de5da711d95e8eea388048b697733
remapthai <remapthai@tutamail.com>
2024-09-11 05:08:38 -0700

    update: font load

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from "vitepress";
 // https://vitepress.dev/reference/site-config
 
 export default defineConfig({
-  base: '/',
+  base: "/",
   title: "Thai ECU Remap",
   description: "รวมข้อมูลรีแมพ ECU มอเตอร์ไซค์",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
 
-    logo: 'logo-mini.webp',
+    logo: "logo-mini.webp",
 
     nav: [
       { text: "หน้าแรก", link: "/" },
@@ -29,13 +29,49 @@ export default defineConfig({
   },
 
   head: [
-    ['script', {}, `
+    [
+      "script",
+      {},
+      `
       if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
           navigator.serviceWorker.register('/service-worker.js');
         });
       }
-    `],
-    ['link', { rel: 'shortcut icon', type:'image/webp', href: '/logo-mini.webp' }]
-  ]
+    `,
+    ],
+    // Preconnect to Google's font domain
+    [
+      "link",
+      {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+        crossorigin: "anonymous",
+      },
+    ],
+
+    // Load Google Fonts asynchronously with fetchpriority
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500&display=swap",
+        media: "print",
+        onload: "this.onload=null;this.removeAttribute('media');",
+        fetchpriority: "high",
+      },
+    ],
+
+    // Fallback for no-JS
+    [
+      "noscript",
+      {},
+      '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500&display=swap">',
+    ],
+
+    [
+      "link",
+      { rel: "shortcut icon", type: "image/webp", href: "/logo-mini.webp" },
+    ],
+  ],
 });
